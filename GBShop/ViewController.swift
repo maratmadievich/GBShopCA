3cda01181e37e8bd5b66db5d8ad40bcc2104e1f0
Marat Nurgaliev <iknow.whoiam@mail.ru>
2019-04-18 18:56:03 +0400

    init

---
+++ GBShop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GBShop
+//
+//  Created by Марат Нургалиев on 18/04/2019.
+//  Copyright © 2019 Марат Нургалиев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ GBShopTests/GBShopTests.swift
@@ -0,0 +1,34 @@
+//
+//  GBShopTests.swift
+//  GBShopTests
+//
+//  Created by Марат Нургалиев on 18/04/2019.
+//  Copyright © 2019 Марат Нургалиев. All rights reserved.
+//
+
+import XCTest
+@testable import GBShop
+
+class GBShopTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
