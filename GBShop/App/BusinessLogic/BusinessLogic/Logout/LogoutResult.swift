12805e7dc6208664f94245e901c4e672409aad77
Marat <iknow.whoiam@mail.ru>
2019-04-23 10:54:24 +0600

    Убрал лишнее из LogoutResult

--- GBShop/App/BusinessLogic/BusinessLogic/Logout/LogoutResult.swift
+++ GBShop/App/BusinessLogic/BusinessLogic/Logout/LogoutResult.swift
@@ -11,29 +11,4 @@ import Foundation
 struct LogoutResult: Codable {
     
     let result: Int
-    
-//    let user: User
 }
-
-//struct User: Codable {
-//    
-//    let id: Int
-//    
-//    let login: String
-//    
-//    let name: String
-//    
-//    let lastname: String
-//    
-//    enum CodingKeys: String, CodingKey {
-//        
-//        case id = "id_user"
-//        
-//        case login = "user_login"
-//        
-//        case name = "user_name"
-//        
-//        case lastname = "user_lastname"
-//    }
-//    
-//}
